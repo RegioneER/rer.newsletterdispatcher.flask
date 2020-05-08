0899f035967ba344bb0093d31bd83bd2263cef2e
Andrea Cecchi <andrea.cecchi85@gmail.com>
2020-05-08 12:23:40 +0200

    fix conf management

--- app.py
+++ app.py
@@ -4,10 +4,13 @@ from flask_mail import Mail
 from flask_restful import Api
 from rq import Queue
 from routes import routes_bp
+from werkzeug.utils import ImportStringError
 
 import os
 import redis
 import logging
+
+logger = logging.getLogger(__name__)
 
 
 def testing_enabled():
@@ -17,15 +20,19 @@ def testing_enabled():
 
 def create_app(debug=True):
     """Create an application."""
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_pyfile('config.cfg', silent=True)
-    app.config['TESTING'] = testing_enabled()
-
+    app = Flask(__name__)
     logging.basicConfig(
         filename='var/log/newsletter_dispatcher.log',
         level=logging.INFO,
         format="[%(asctime)s] %(levelname)s - %(message)s",
     )
+    try:
+        app.config.from_object('aconfig.Config')
+    except ImportStringError:
+        logger.error(
+            "No config.py file found. Starting with stadndard configuration."
+        )
+    app.config['TESTING'] = testing_enabled()
 
     app.redis = redis.Redis()
     app.task_queue = Queue(connection=app.redis, default_timeout=6000)
