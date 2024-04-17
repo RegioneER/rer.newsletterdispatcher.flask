9fee706164ecc7f9b59509ae52340f9fd1bbe323
Roman <roman.kysil@redturtle.it>
2024-04-17 15:31:01 +0200

    Update var list

--- config.py
+++ config.py
@@ -3,6 +3,9 @@ import os
 
 MAIL_SERVER = os.environ.get("RER_NEWSLETTERDISPATCHER_MAIL_SERVER", default="")
 SENTRY_DSN = os.environ.get("RER_NEWSLETTERDISPATCHER_SENTRY_DSN", default="")
+MAIL_PORT = os.environ.get("RER_NEWSLETTERDISPATCHER_MAIL_PORT", default=25)
+MAIL_USERNAME = os.environ.get("RER_NEWSLETTERDISPATCHER_MAIL_USERNAME", default="")
+MAIL_PASSWORD = os.environ.get("RER_NEWSLETTERDISPATCHER_MAIL_PASSWORD", default="")
 
 # NOTE: Redis default port is 6379
-REDIS_PORT = os.environ.get("RER_NEWSLETTERDISPATCHER_REDIS_PORT", default=6379)
+REDIS_PORT = os.environ.get("RER_NEWSLETTERDISPATCHER_REDIS_PORT", default="6379")
